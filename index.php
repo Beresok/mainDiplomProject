cec5e7008a8467d8f2742bcca0eca7ae939da11e
Evgeni Beresnew <dzakob@mail.ru>
2017-05-17 03:35:05 +0300

    Начало работы

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<? echo '<h1> Привет диплом! </h1>'; ?>
+<?  //FRONT CONTROLLER
+    // 1. Общие настройки
+        ini_set('display_errors',1);
+        error_reporting(E_ALL);
+    // 2. Подключение файлов системы
+        define('ROOT', dirname(__FILE__));
+        require_once (ROOT.'/components/Router.php');
+    // 3. Установка соединения с БД
+    // 4. Вызов Router
+
+?>
