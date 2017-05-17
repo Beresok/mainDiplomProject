9bffdaf3d014ea54a32d89aceaa087d878d03c1a
Evgeni Beresnew <dzakob@mail.ru>
2017-05-17 18:23:30 +0300

    actual branch

--- models/News.php
+++ models/News.php
@@ -2,11 +2,11 @@
     class News {
 
         public static function getNewsById($id){
-
+            // просмотр новости детально
         }
 
         public static function getNewsList(){
-
+            // просмотр списка новостей
         }
 
     }
