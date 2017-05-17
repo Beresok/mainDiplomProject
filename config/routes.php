ca71f242f7d24019254b91333e0c5550dd12d1d4
Evgeni Beresnew <dzakob@mail.ru>
2017-05-17 18:31:20 +0300

    test commit 1

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,7 @@
 <?php
     return array(
         'news/([a-z]+)/([0-9]+)' => 'news/view/$1/$2',
+// test commit 1
 //        'news/([0-9]+)' => 'news/view',
 //        'news' => 'news/index',
     )
